3d1b02a38df5c7ef9fb01c44efbc442eba253c70
Wanderley01 <edioneto00@gmail.com>
2024-12-23 15:33:15 -0300

    Algumas mudanças e paginação

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,13 +1,25 @@
 import React from "react";  
+import back from '../img/botao-voltar.png';
 import { useNavigate } from 'react-router-dom'; 
+import '../css/dex.css'; 
 function Dex() {
-
+    const navigate = useNavigate();
 
     return (
         <div>
             
-            <div>
-                <h1>Pokedex</h1>
+            <div className="back">
+            <button onClick={() => (navigate('/'))}>
+                <img  src={back} width={20}/>
+            </button>
+            </div>
+
+            <div className="querybar">
+
+            </div>
+
+            <div className="dex">
+
             </div>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Dex from "./Dex.jsx";
 import "../css/home.css";
 import Red from "../img/Red.png";
 import { useNavigate } from 'react-router-dom';
+import char from "../img/char.png";
 
 function Home() {
 
@@ -10,22 +11,29 @@ function Home() {
 
 
     return(
-        <div className="box">
-            <div className="title">
-                <h1> POKEDEX </h1>
-            </div>
-            
-            <div className="foto">
-                <img src={Red} alt="Red" width={100}/>
-            </div>
+            <div className="container">
+                    <div className="box">
+                <div className="title">
+                    <h1> POKEDEX </h1>
+                </div>
+                
+                <div className="Char">
+                <img src={char} alt="Red"/>
+                </div>
 
-            <div className="options">
-                <button onClick={() => (navigate('/Dex'))}>Pokemons</button>
-                <button onClick={() => (navigate('/#'))}>nada</button>
-                <button onClick={() => (navigate('/#'))}>nada</button>
-                <button onClick={() => (navigate('/#'))}>nada</button>
+                <div className="foto">
+                    <img src={Red} alt="Red" width={100}/>
+                </div>
+
+                <div className="options">
+                    <button onClick={() => (navigate('/Dex'))}>Pokemons</button>
+                    <button onClick={() => (navigate('/#'))}>nada</button>
+                    <button onClick={() => (navigate('/#'))}>nada</button>
+                    <button onClick={() => (navigate('/#'))}>nada</button>
+                </div>
             </div>
         </div>
+        
     ) 
 };
 
